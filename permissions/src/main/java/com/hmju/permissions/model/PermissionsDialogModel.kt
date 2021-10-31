b5c7639d0fedb0765be36163de8d13ed7d49b2e9
SieunJu <j.sieun73@gmail.com>
2021-10-31 17:25:07 +0900

    fix: Parcelable 제거

--- permissions/src/main/java/com/hmju/permissions/model/PermissionsDialogModel.kt
+++ permissions/src/main/java/com/hmju/permissions/model/PermissionsDialogModel.kt
@@ -1,14 +1,10 @@
 package com.hmju.permissions.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
 /**
  * Description : 권한 거부시 노출하는 팝업의 데이터 모델
  *
  * Created by juhongmin on 10/31/21
  */
-@Parcelize
 data class PermissionsDialogModel(
     var title: String? = null,
     var contents: String? = null,
@@ -16,4 +12,4 @@ data class PermissionsDialogModel(
     var rightButton: String? = null,
     var isSettingWhich: Int = -1,
     var packageName : String? = null
-) : Parcelable
+)
--- permissions/src/main/java/com/hmju/permissions/ui/PermissionsActivity.kt
+++ permissions/src/main/java/com/hmju/permissions/ui/PermissionsActivity.kt
@@ -1,17 +1,12 @@
 package com.hmju.permissions.ui
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.hmju.permissions.ExtraCode
 import com.hmju.permissions.SimplePermissions
-import com.hmju.permissions.model.PermissionsDialogModel
-import com.hmju.permissions.model.PermissionsDialogUiModel
 
 /**
  * Description : 권한 Activity
@@ -25,15 +20,6 @@ class PermissionsActivity : AppCompatActivity() {
 	 */
 	private val permissionsResult = registerForActivityResult(
 			ActivityResultContracts.RequestMultiplePermissions()
-	) { _ ->
-		handleCallback()
-	}
-
-	/**
-	 * 권한 설정 페이지 진입후 결과값 리턴 받는 부분
-	 */
-	private val settingsResult = registerForActivityResult(
-			ActivityResultContracts.StartActivityForResult()
 	) { _ ->
 		handleCallback()
 	}
@@ -54,78 +40,12 @@ class PermissionsActivity : AppCompatActivity() {
 	}
 
 	/**
-	 * 권한 결과에 맞게 처리하는 함수
-	 * @param permissions 권한 결과에 대한 Result 값
-	 */
-	private fun handlePermissions(permissions: Map<String, Boolean>) {
-		val negativeList = arrayListOf<String>()
-		for (key in permissions.keys) {
-			if (permissions[key] == false) {
-				negativeList.add(key)
-			}
-		}
-
-		val isAllGranted = negativeList.size == 0
-
-		// 모두 승인한 상태인경우
-		if (isAllGranted) {
-			handleCallback()
-		} else {
-			// 거절시 다이얼로그 팝업
-			intent.getParcelableExtra<PermissionsDialogModel>(ExtraCode.NEGATIVE_DIALOG)?.let { dialogModel ->
-				PermissionsDialog(
-						this,
-						intent.getParcelableExtra(ExtraCode.NEGATIVE_UI_DIALOG)
-								?: PermissionsDialogUiModel())
-						.setTitle(dialogModel.title)
-						.setContents(dialogModel.contents)
-						.setNegativeButton(dialogModel.leftButton)
-						.setPositiveButton(dialogModel.rightButton)
-						.show { which ->
-							// 설정 화면으로 이동.
-							if (which == dialogModel.isSettingWhich) {
-								if (!movePermissionsSetting(dialogModel.packageName)) {
-									handleCallback()
-								}
-							} else {
-								handleCallback()
-							}
-						}
-			} ?: run {
-				handleCallback()
-			}
-		}
-	}
-
-	/**
 	 * SimplePermissions 클래스에 리스너 전달 처리 함수
 	 */
 	private fun handleCallback() {
 		finish()
 		Handler(Looper.getMainLooper()).post {
 			SimplePermissions.listener?.onResult()
-		}
-	}
-
-	/**
-	 * 권한 페이지 이동
-	 * @param packageName AppModule Package Name
-	 * @return true 정상적으로 권한페이지로 이동처리된 경우, false 권한 페이지로 이동이 안된 경우
-	 */
-	private fun movePermissionsSetting(packageName: String?): Boolean {
-		if (packageName.isNullOrEmpty()) return false
-
-		return try {
-			Intent(
-					Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-					Uri.parse("package:${packageName}")
-			).apply {
-				addCategory(Intent.CATEGORY_DEFAULT)
-				settingsResult.launch(this)
-			}
-			true
-		} catch (ex: Exception) {
-			false
 		}
 	}
 
